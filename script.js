0e6cf4d4abeccfc4cb93ea5074510138e21fb4dc
marcingal <marcin921125@gmail.com>
2018-11-05 13:40:04 +0100

    second exercise

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
 
-// 1)
-// function Cat(){
-//     this.makeSound = () => console.log('Meowww!')
-// }
-
-
-// const cat = new Cat()
-
-// cat.makeSound()
-
-// class Cat {
-//     makeSound() { console.log('Meowww!') }
-// }
-
-// const cat = new Cat()
-// cat.makeSound()
-
-makeCat = () => {
-    return {
+const makeCat = () => ({
         makeSound: () => console.log('Meow!')
-    }
-}
+    })
+
 const cat = makeCat();
 cat.makeSound()
