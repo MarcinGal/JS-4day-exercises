1cf74451b98db076f5116e690d605a8940a009af
marcingal <marcin921125@gmail.com>
2018-11-05 14:10:03 +0100

    3exercise in function method

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
-class Cat {
-    constructor() {
-        this.sound = 'Meoww!';
-    }
-    makeSound() { console.log(this.sound) };
-    changeSound(sound) {this.sound = sound };
-}
+// class Cat {
+//     constructor() {
+//         this.sound = 'Meoww!';
+//     }
+//     makeSound() { console.log(this.sound) };
+//     changeSound(sound) {this.sound = sound };
+// }
 
-const cat = new Cat()
+// const cat = new Cat()
+
+// cat.makeSound()
+
+// cat.changeSound('Kłaczek')
+
+// cat.makeSound()
+
+function Cat(){this.sound = 'Meoww!'};
+Cat.prototype.makeSound = function(){ console.log(this.sound) };
+Cat.prototype.changeSound= function(sound) {this.sound = sound };
+
+const cat = new Cat;
 
 cat.makeSound()
 
